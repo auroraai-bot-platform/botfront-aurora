7ac2698075e89c469005ee16cc611f5856f7136a
Johannes Feig <johannes.feig@gofore.com>
2021-06-15 13:13:17 +0300

    test: create tests for the users endpoint

---
+++ botfront/cypress/integration/rest/users.spec.js
@@ -0,0 +1,95 @@
+/* global cy:true */
+const token = Cypress.env('REST_API_TOKEN');
+
+const endpoint = '/api/users';
+const email = 'test@example.org';
+const password = 'Aaaaaaaa00';
+
+
+describe('user endpoint basic functionality', () => {
+  before(() => {
+    cy.deleteUser(email);
+  });
+
+  after(() => {
+    cy.deleteUser(email);
+  });
+
+  it('it should respond with error missing token', () => {
+    cy.request({
+      url: endpoint,
+      method: 'PUT',
+      failOnStatusCode: false
+    })
+    .as('users');
+
+    cy.get('@users').should((res) => {
+      expect(res.status).to.eq(403);
+    });
+  });
+
+  it('should respond with error invalid json', () => {
+    cy.request({
+      url: endpoint,
+      method: 'PUT',
+      headers: { Authorization: token },
+      body: 'test',
+      failOnStatusCode: false
+    })
+    .as('users');
+
+    cy.get('@users').should((res) => {
+      expect(res.status).to.eq(400);
+    });
+  });
+
+  it('Create valid user', () => {
+    cy.request({
+      url: endpoint,
+      method: 'PUT',
+      headers: { Authorization: token },
+      body: { email, password },
+      failOnStatusCode: false
+    })
+    .as('users');
+
+    cy.get('@users').should((res) => {
+      expect(res.status).to.eq(200);
+    });
+  });
+});
+
+describe('user endpoint fails on duplicate', () => {
+
+  before(() => {
+    cy.deleteUser(email);
+  });
+
+  after(() => {
+    cy.deleteUser(email);
+  });
+
+  it('should fail on second request', () => {
+    cy.request({
+      url: endpoint,
+      method: 'PUT',
+      headers: { Authorization: token },
+      body: { email, password },
+      failOnStatusCode: false
+    })
+    .should((res) => {
+      expect(res.status).to.eq(200);
+    });
+
+    cy.request({
+      url: endpoint,
+      method: 'PUT',
+      headers: { Authorization: token },
+      body: { email, password },
+      failOnStatusCode: false
+    })
+    .should((res) => {
+      expect(res.status).to.eq(500);
+    });
+  });
+});
