09db631380e1bd65c3ee9a041f2b30a91d58c4e3
Johannes Feig <johannes.feig@gofore.com>
2021-09-07 15:45:57 +0300

    feat: REST API uses different port than botfront
    The REST API runs now on a separate port.
    This makes it easier to limit access based on ports.
    The port can be passed as an environment variable `REST_API_PORT` to the container.

--- botfront/imports/api/rest/utilities.service.js
+++ botfront/imports/api/rest/utilities.service.js
@@ -10,20 +10,6 @@ export function getUser() {
   };
 }
 
-
-export function fetchBodyMW(req, res, next) {
-  let body = "";
-
-  req.on('data', Meteor.bindEnvironment(function (data) {
-    body += data;
-  }));
-
-  req.on('end', Meteor.bindEnvironment(function () {
-    req.body = body;
-    next();
-  }));
-}
-
 export function authMW(token) {
   return (function (req, res, next) {
     if (token == null || token.length < 1) {
