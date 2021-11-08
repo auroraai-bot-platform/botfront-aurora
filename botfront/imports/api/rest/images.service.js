ad66ae10535bfb04401fb2b7a7ee0eb1dc07c23d
Johannes Feig <johannes.feig@gofore.com>
2021-11-08 15:25:18 +0200

    tests: add integration tests for the image endpoints

--- botfront/imports/api/rest/images.service.js
+++ botfront/imports/api/rest/images.service.js
@@ -1,4 +1,4 @@
-import { S3 } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 import { getS3Url } from './utilities.service';
 import { region } from './index';
 
@@ -8,13 +8,13 @@ export async function uploadImage(outputBucket, key, data) {
 
   const fileUrl = getS3Url(region, outputBucket, key);
 
-  const s3 = new S3({ region });
-  await s3.putObject({ Bucket: outputBucket, Key: key, Body: buffer });
+  const s3 = new S3Client({ region });
+  await s3.send(new PutObjectCommand({ Bucket: outputBucket, Key: key, Body: buffer }));
 
   return fileUrl;
 }
 
 export async function deleteImage(bucket, key) {
-  const s3 = new S3({ region });
-  await s3.deleteObject({ Bucket: bucket, Key: key });
+  const s3 = new S3Client({ region });
+  await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key }));
 }
