9016b4a21ce37c27aa611319093b76e3202cc0c2
Tuomas Paulin <tuomas.paulin@gofore.com>
2022-01-18 09:38:41 +0200

    chore: cleaned unneeded variables

--- botfront/imports/api/rest/files.service.test.js
+++ botfront/imports/api/rest/files.service.test.js
@@ -1,20 +1,13 @@
 import { expect, assert } from 'chai';
 import { mockClient } from 'aws-sdk-client-mock';
 import { S3Client } from '@aws-sdk/client-s3';
-import { uploadFile } from './files.service';
 import {promises as fs} from 'fs';
 import {c} from 'tar';
 import axios from 'axios';
 
 const baseUrl = 'http://localhost:3030/api';
 
-const outputBucket = 'test-bucket-123';
-const key = 'testfile.png';
-const buffer = Buffer.from('HELLO WORLD');
-const data = buffer.toString('Base64');
-
 let client;
-
 describe('image rest api tests', () => {
   before(() => {
     client = mockClient(S3Client);
@@ -32,8 +25,8 @@ describe('image rest api tests', () => {
 
     const uploadResult = await axios.post(`${baseUrl}/images`, uploadData);
 
-    assert.equal(uploadResult.status, 200);
-    assert.isString(uploadResult.data?.uri);
+    assert.equal(uploadResult.status, 200, 'Upload failed');
+    assert.isString(uploadResult.data?.uri, 'Incorrect uri returned');
 
     const deleteData = {
       projectId: 'test',
