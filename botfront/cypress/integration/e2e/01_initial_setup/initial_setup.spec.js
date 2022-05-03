8aaf9ba026453d61b070461936b294f0b8efa41f
Henriikka Mäkelä <henriikka.makela@alten.fi>
2022-05-03 14:23:18 +0300

    refactor: refactored initial setup tests and changed folder structure

--- botfront/cypress/integration/e2e/01_initial_setup/initial_setup.spec.js
+++ botfront/cypress/integration/e2e/01_initial_setup/initial_setup.spec.js
@@ -2,8 +2,10 @@ describe('Initial setup', () => {
     before(() => {
         cy.exec('docker exec botfront-mongo mongo bf --host localhost:27017 --eval "db.dropDatabase();"')
         .its('stdout').should('contain', '{ "ok" : 1 }')
+        // Wiping the db also wipes the indexes we need to recreate those
         cy.exec('docker exec botfront-mongo mongo bf --host localhost:27017 --eval "db.botResponses.createIndex({key:1, projectId:1}, {unique: true});"')
         .its('stdout').should('contain', '{\n\t"numIndexesBefore" : 1,\n\t"numIndexesAfter" : 2,\n\t"createdCollectionAutomatically" : true,\n\t"ok" : 1\n}')
+        cy.waitForResolve(Cypress.env('baseUrl'))
         cy.visit('/')
         cy.get('[data-cy="start-setup"]').click()
         cy.get('[data-cy="account-step"]').should('contain', 'Create your admin account')
@@ -16,16 +18,16 @@ describe('Initial setup', () => {
     })
 
     it('Create admin account', () => {
-        cy.get('#uniforms-0001-0001').type('Test')
-        cy.get('#uniforms-0001-0003').type('Tester')
+        cy.get('#uniforms-0001-0001').type('Testing')
+        cy.get('#uniforms-0001-0003').type('McTest')
         cy.get('#uniforms-0001-0005').type('test@test.com')
         cy.get('#uniforms-0001-0007').type('Aaaaaaaa00')
         cy.get('#uniforms-0001-0009').type('Aaaaaaaa00')
         cy.get('[data-cy="account-create-button"]').click()
     })
 
-    it('Create testbot project', () => {
+    it('Create project', () => {
         cy.deleteProject('chitchat-*')
-        cy.createProject('bf', 'testbot', 'en')
+        cy.createProject('bf', 'trial', 'en')
     })
 })
