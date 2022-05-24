0e3f6d7bc671d4c50dcf095c7fcea51f296f5fa4
Henriikka Mäkelä <henriikka.makela@alten.fi>
2022-05-24 12:13:22 +0300

    fix: fixed and refactored projects.spec.js tests

--- botfront/cypress/integration/e2e/admin_dashboard/projects.spec.js
+++ botfront/cypress/integration/e2e/admin_dashboard/projects.spec.js
@@ -1,45 +1,38 @@
-/* eslint-disable no-undef */
-describe('project creation', function () {
-    beforeEach(function () {
-        cy.createProject('bf', 'Duedix', 'fr');
-        cy.visit('/login');
-        cy.login();
-    });
+describe('Project creation', () => {
 
-    afterEach(function () {
-        cy.deleteProject('bf');
-        cy.deleteProject('test');
-        cy.deleteProject('test1');
-    });
+    before(() => {
+        cy.login()
+        cy.deleteProject('bf')
+        cy.createProject('bf', 'My Project', 'en')
+    })
 
-    it('should be possible to create and delete project', function () {
-        cy.visit('/admin/projects');
-        cy.wait(1000);
-        cy.dataCy('new-project').click();
-        cy.get('#uniforms-0000-0001').type('test');
-        cy.get('#uniforms-0000-0003').type('bf-test');
-        cy.get('#uniforms-0000-0004').click();
-        cy.get('#uniforms-0000-0004')
-            .children()
-            .children()
-            .first()
-            .click();
-        cy.dataCy('submit-field').click();
-        cy.location('href').should('match', /http:\/\/\w*:*[0-9]*\/admin\/projects/);
-        cy.get(':nth-child(3) > .rt-tr > :nth-child(1)').should('have.text', 'test');
-        cy.get(':nth-child(3) > .rt-tr > :nth-child(3) a').click();
-        cy.location('href').should('match', /http:\/\/\w*:*[0-9]*\/admin\/project\/./);
-        cy.dataCy('delete-project').should('be.disabled');
-        cy.get('.ui > label').click();
-        cy.dataCy('submit-field').click();
-        cy.get(':nth-child(1) > .rt-tr > :nth-child(1)').eq(1).should('have.text', 'test');
-        cy.get(':nth-child(1) > .rt-tr > :nth-child(3) a').click();
-        cy.location('href').should('match', /http:\/\/\w*:*[0-9]*\/admin\/project\/./);
-        cy.dataCy('delete-project').should('not.be.disabled');
-        cy.dataCy('delete-project').click();
-        cy.get('.primary').click();
-        cy.get(':nth-child(3) > .rt-tr > :nth-child(1)').should('have.text', '\u00a0'); // \u00a0 is nbsp, the 'empty' value in the table
-        cy.get(':nth-child(2) > .rt-tr > :nth-child(1)').should('have.text', 'Duedix');
-        cy.get(':nth-child(1) > .rt-tr > :nth-child(1)').eq(1).should('have.text', 'Chitchat');
-    });
-});
+    afterEach(() => {
+        cy.deleteProject('bf')
+        cy.get('@testProjectId').invoke('text').then((projectId) => {
+            cy.deleteProject(projectId)
+        })
+    })
+
+    it('Create and delete project', () => {
+        cy.visit('/admin/projects')
+        cy.dataCy('new-project').click()
+        cy.get('#uniforms-0000-0001').type('test')
+        cy.get('#uniforms-0000-0003').type('bf-test')
+        cy.get('#uniforms-0000-0004').click()
+        cy.get('#uniforms-0000-0004').children().children().first().click()
+        cy.dataCy('submit-field').click()
+        cy.get(':nth-child(2) > .rt-tr > :nth-child(1)').should('have.text', 'test')
+        cy.get('.rt-tbody > :nth-child(2) > .rt-tr > :nth-child(2)').as('testProjectId') //creating alias for project id which is used in afterEach
+        cy.get(':nth-child(2) > .rt-tr > :nth-child(3) a').click()
+        cy.dataCy('delete-project').should('be.disabled')
+        cy.get('.ui > label').click()
+        cy.dataCy('submit-field').click()
+        cy.get(':nth-child(1) > .rt-tr > :nth-child(1)').eq(1).should('have.text', 'test')
+        cy.get(':nth-child(1) > .rt-tr > :nth-child(3) a').click()
+        cy.dataCy('delete-project').should('not.be.disabled')
+        cy.dataCy('delete-project').click()
+        cy.get('.primary').click()
+        cy.get(':nth-child(2) > .rt-tr > :nth-child(1)').should('not.have.text', 'test')
+        cy.get('.rt-tbody > :nth-child(1) > .rt-tr > :nth-child(1)').should('have.text', 'My Project')
+    })
+})
