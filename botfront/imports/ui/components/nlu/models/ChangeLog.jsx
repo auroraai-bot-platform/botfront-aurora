5cd512b6fede65307bdcf65e1acd56ae50551f92
Johannes Feig <johannes.feig@gofore.com>
2022-06-20 10:41:03 +0300

    chore: add react table

--- botfront/imports/ui/components/nlu/models/ChangeLog.jsx
+++ botfront/imports/ui/components/nlu/models/ChangeLog.jsx
@@ -1,25 +1,38 @@
-import React, {
-    useContext, useMemo, useEffect, useState,
-} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Statistic, Button } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { connect } from 'react-redux';
-import { useQuery } from '@apollo/react-hooks';
-import { saveAs } from 'file-saver';
-import { Stories as StoriesCollection } from '../../../../api/story/stories.collection';
-import { Loading } from '../../utils/Utils';
-import IntentLabel from '../common/IntentLabel';
-import DataTable from '../../common/DataTable';
-import { ProjectContext } from '../../../layouts/context';
-import { GET_INTENT_STATISTICS, GET_EXAMPLE_COUNT, GET_ENTITY_STATISTICS } from './graphql';
+
+import ReactTable from 'react-table-v6';
 import { Changes } from '../../../../api/changes/changes.collection';
 
 const ChangeLog = (props) => {
-    const { changes, projectId } = props;
+    const columns = [
+        { Header: 'projectId', accessor: 'projectId', id: 'projectId' },
+        { Header: 'updatedAt', accessor: 'updatedAt', id: 'updatedAt' },
+    ];
+
+    const data = [
+        { projectId: 1, updatedAt: 2 },
+    ];
+
+    const { changes } = props;
+    const data2 = changes.map(({ projectId, updatedAt }) => {
+        return { projectId, updatedAt };
+    });
+    console.log(data2);
+    console.log(data);
     // const [changes, setChanges] = useState([]);
 
-    return <h1>Hello World {JSON.stringify(changes)}</h1>;
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <ReactTable
+                data={data2}
+                columns={columns}
+            />
+        </div>
+    );
 };
 
 
