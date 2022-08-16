893663e5870ea7db8c159375f7f4ce1da3beab09
Henriikka Mäkelä <henriikka.makela@alten.fi>
2022-08-16 13:49:10 +0300

    fix: fixed and refactored nlu models tests

--- botfront/cypress/integration/e2e/nlu_models/nlu_models.spec.js
+++ botfront/cypress/integration/e2e/nlu_models/nlu_models.spec.js
@@ -1,57 +1,53 @@
-/* global cy:true */
+describe('NLU Models ', () => {
+    before(() => {
+        cy.login()
+        cy.deleteProject('bf')
+        cy.createProject('bf', 'My Project', 'en')
+    })
 
-describe('NLU Models ', function() {
-    beforeEach(function() {
-        cy.login();
-    });
+    beforeEach(() => {
+        cy.login()
+        cy.visit('/project/bf/settings')
+    })
 
-    before(function() {
-        cy.createProject('bf', 'My Project', 'en');
-    });
+    after(() => {
+        cy.deleteProject('bf')
+    })
 
-    after(function() {
-        cy.deleteProject('bf');
-    });
+    it('create a new model by adding a new language to the project and then delete it', () => {
+        cy.get('[data-cy=language-selector]').click()
+        cy.get('[data-cy=language-selector] input').type('French{enter}')
+        cy.dataCy('settings-menu').find('.active.item').click()
+        cy.get('[data-cy=save-changes]').click()
+        cy.get('.s-alert-success').should('be.visible')
+        cy.visit('/project/bf/nlu/models')
 
-    it('should be able to create a new Model by adding a new language to the project and then delete it', function() {
-        cy.visit('/project/bf/settings');
-        cy.get('[data-cy=language-selector]').click();
-        cy.get('[data-cy=language-selector] input').type('French{enter}');
-        cy.dataCy('settings-menu').find('.active.item').click();
-        cy.get('[data-cy=save-changes]').click();
-        cy.get('.s-alert-success').should('be.visible');
-        cy.visit('/project/bf/nlu/models');
         // Instance should also be added to the model that is created.
-        cy.get('[data-cy=example-text-editor-input]').should('exist');
-        cy.get('[data-cy=language-selector]').click();
-        cy.get('[data-cy=language-selector] input').type('French{enter}');
-        cy.dataCy('nlu-menu-settings').click();
-        cy.contains('Delete').click();
-        cy.get('.dowload-model-backup-button').click();
-        cy.get('.delete-model-button').click();
-        cy.get('.ui.page.modals .primary').click();
-        cy.get('[data-cy=language-selector]').click();
-        cy.get('[data-cy=language-selector] input').type('Fre');
-        cy.get('[data-cy=language-selector]')
-            .contains('French')
-            .should('not.exist');
-    });
+        cy.get('[data-cy=example-text-editor-input]').should('exist')
+        cy.get('[data-cy=language-selector]').click()
+        cy.get('[data-cy=language-selector] input').type('French{enter}')
+        cy.dataCy('nlu-menu-settings').click()
+        cy.contains('Delete').click()
+        cy.get('.dowload-model-backup-button').click()
+        cy.get('.delete-model-button').click()
+        cy.get('.ui.page.modals .primary').click()
+        cy.get('[data-cy=language-selector]').click({timeout: 5000})
+        cy.get('[data-cy=language-selector] input').type('Fre')
+        cy.get('[data-cy=language-selector]').contains('French').should('not.exist')
+    })
 
-    it('should NOT be able to delete the default model', function() {
-        cy.visit('/project/bf/settings');
-        cy.get('[data-cy=default-langauge-selection] .ui > .search').click();
-        cy.get('[data-cy=default-langauge-selection] input').type('English');
-        cy.get('[data-cy=default-langauge-selection]')
-            .contains('English')
-            .click({ force: true });
-        cy.dataCy('settings-menu').find('.active.item').click();
-        cy.get('[data-cy=default-langauge-selection]').click();
+    it('deleting the default model should not be possible', () => {
+        cy.get('[data-cy=default-langauge-selection] .ui > .search').click()
+        cy.get('[data-cy=default-langauge-selection] input').type('English')
+        cy.get('[data-cy=default-langauge-selection]').contains('English').click({ force: true })
+        cy.dataCy('settings-menu').find('.active.item').click()
+        cy.get('[data-cy=default-langauge-selection]').click()
 
         // Try to delete the default model
-        cy.visit('/project/bf/nlu/models');
-        cy.dataCy('nlu-menu-settings').click();
-        cy.contains('Delete').click();
-        cy.get('.dowload-model-backup-button').click();
-        cy.get('.delete-model-button').should('not.exist');
-    });
-});
+        cy.visit('/project/bf/nlu/models')
+        cy.dataCy('nlu-menu-settings').click()
+        cy.contains('Delete').click()
+        cy.get('.dowload-model-backup-button').click()
+        cy.get('.delete-model-button').should('not.exist')
+    })
+})
